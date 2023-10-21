c120ebe360361ef34d8a55ea5293db68bf1c69cc
Abzhan Shynggys <145852140+ShynggysA@users.noreply.github.com>
2023-10-21 13:33:21 +0600

    Update 3.py

--- РК/3.py
+++ РК/3.py
@@ -1,1 +1,27 @@
+def generate_fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [1]
+    elif n == 2:
+        return [1, 1]
+    else:
+        fibonacci_sequence = [1, 1]
+        for i in range(2, n):
+            next_term = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_term)
+        return fibonacci_sequence
+
+while True:
+    try:
+        length = int(input("Please enter the length of the Fibonacci sequence: "))
+        if length < 0:
+            print("Please enter a positive number.")
+        else:
+            fibonacci_sequence = generate_fibonacci(length)
+            print(f"The Fibonacci sequence for {length} is:")
+            print(", ".join(map(str, fibonacci_sequence)))
+            break
+    except ValueError:
+        print("Invalid input. Please enter a valid positive integer.")
 
