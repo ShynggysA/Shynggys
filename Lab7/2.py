c339f8a444624374f0b17c588b41be19e4520de3
Abzhan Shynggys <145852140+ShynggysA@users.noreply.github.com>
2023-11-12 04:10:59 +0600

    Update 2.py

--- Lab7/2.py
+++ Lab7/2.py
@@ -1,6 +1,6 @@
 import random
  
-file = open('11.txt', encoding='utf-8')
+file = open('c:\\LAB_Python\\Shynggys\\Lab7\\11.txt', encoding='utf-8')
 data = file.read()
 lines = data.split('\n')
 line = random.randrange(len(lines))
