3282c26c5225a18f2d7655dffa230ec53650ab18
Abzhan Shynggys <145852140+ShynggysA@users.noreply.github.com>
2023-11-12 04:12:41 +0600

    Update 7.py

--- Lab7/7.py
+++ Lab7/7.py
@@ -1,10 +1,10 @@
 from collections import Counter
 
 def func(f):
-    with open('11.txt', 'r', encoding='utf-8') as file:
+    with open('c:\\LAB_Python\\Shynggys\\Lab7\\11.txt', 'r', encoding='utf-8') as file:
         words = file.read().split()
         word_freq = Counter(words)
     return word_freq
 
-result = func("11.txt")
+result = func("c:\\LAB_Python\\Shynggys\\Lab7\\11.txt")
 print(result)
