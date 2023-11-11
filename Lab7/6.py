29f7ab63d3aac90d9f07ff5f8d6c6657a36ada4a
Abzhan Shynggys <145852140+ShynggysA@users.noreply.github.com>
2023-11-12 03:28:15 +0600

    Create 6.py

---
+++ Lab7/6.py
@@ -0,0 +1,12 @@
+def func(f):
+    count_all = 0
+    count_none = 0
+    with open('11.txt', 'r', encoding='utf-8') as file:
+        words = file.read().split()
+        count_all = words.count("all")
+        count_none = words.count("none")
+    return count_all, count_none
+
+result_all, result_none = func("11.txt")
+print(f"Number of 'all' words: {result_all}")
+print(f"Number of 'none' words: {result_none}")
