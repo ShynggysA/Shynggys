4c818db45f2fbfc3c143c90b8094724a051b7539
Abzhan Shynggys <145852140+ShynggysA@users.noreply.github.com>
2023-11-12 04:12:57 +0600

    Update 8.py

--- Lab7/8.py
+++ Lab7/8.py
@@ -1,8 +1,8 @@
 def func(f):
-    with open('11.txt', 'r', encoding='utf-8') as file:
+    with open('c:\\LAB_Python\\Shynggys\\Lab7\\11.txt', 'r', encoding='utf-8') as file:
         words = file.read().split()
         longest_word = max(words, key=len)
     return longest_word
 
-result = func("11.txt")
+result = func("c:\\LAB_Python\\Shynggys\\Lab7\\11.txt")
 print(f"The longest word is: {result}")
