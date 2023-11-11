679e9684d83c8dcb83e6fbc99066eb9e7d155d51
Abzhan Shynggys <145852140+ShynggysA@users.noreply.github.com>
2023-11-12 04:11:20 +0600

    Update 3.py

--- Lab7/3.py
+++ Lab7/3.py
@@ -1,9 +1,9 @@
 def func(f):
     count = 0
-    with open('11.txt', 'r', encoding='utf-8') as file:
+    with open('c:\\LAB_Python\\Shynggys\\Lab7\\11.txt', 'r', encoding='utf-8') as file:
         for line in file:
             count += sum(1 for char in line if char.isupper())
     return count
 
-result = func("text1.txt")
+result = func("c:\\LAB_Python\\Shynggys\\Lab7\\11.txt")
 print(f"Uppercase characters count: {result}")
