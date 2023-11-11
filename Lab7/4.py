2ceeaed349bf495ef8577e5abd30f41c0978c478
Abzhan Shynggys <145852140+ShynggysA@users.noreply.github.com>
2023-11-12 04:11:35 +0600

    Update 4.py

--- Lab7/4.py
+++ Lab7/4.py
@@ -1,9 +1,9 @@
 def func(f):
     count = 0
-    with open('11.txt', 'r', encoding='utf-8') as file:
+    with open('c:\\LAB_Python\\Shynggys\\Lab7\\11.txt', 'r', encoding='utf-8') as file:
         words = file.read().split()
         count += sum(1 for word in words if word.endswith(('F', 'f')))
     return count
 
-result = func("11.txt")
+result = func("c:\\LAB_Python\\Shynggys\\Lab7\\11.txt")
 print(f"Number of words ending with 'F': {result}")
