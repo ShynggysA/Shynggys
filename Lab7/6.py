1a6a63282b2e9540db8bbf9091f5793c68c6c76c
Abzhan Shynggys <145852140+ShynggysA@users.noreply.github.com>
2023-11-12 04:12:23 +0600

    Update 6.py

--- Lab7/6.py
+++ Lab7/6.py
@@ -1,12 +1,12 @@
 def func(f):
     count_all = 0
     count_none = 0
-    with open('11.txt', 'r', encoding='utf-8') as file:
+    with open('c:\\LAB_Python\\Shynggys\\Lab7\\11.txt', 'r', encoding='utf-8') as file:
         words = file.read().split()
         count_all = words.count("all")
         count_none = words.count("none")
     return count_all, count_none
 
-result_all, result_none = func("11.txt")
+result_all, result_none = func("c:\\LAB_Python\\Shynggys\\Lab7\\11.txt")
 print(f"Number of 'all' words: {result_all}")
 print(f"Number of 'none' words: {result_none}")
